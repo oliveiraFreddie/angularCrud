5b4c5f4184a49bb7da1f4c291b6d4ab7d023b1fc
oliveiraFreddie <freddie.oliveira95@gmail.com>
2024-01-24 12:00:48 +0000

    headerService

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { HeaderService } from '../../components/template/header/header.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  constructor(private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Início',
+      icon: 'home',
+      routeUrl: ''
+    }
+  }
+  ngOnInit(): void {}
 }
